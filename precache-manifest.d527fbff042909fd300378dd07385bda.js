bef2748afd661b7d4750b5e7e0e082506742bbdf
artemartem <miraclemarvel118@gmail.com>
2020-05-27 16:14:02 +0300

    Updates

---
+++ precache-manifest.d527fbff042909fd300378dd07385bda.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485df9046b7338a9c8bbac34f65a3b3f",
+    "url": "/first-project/index.html"
+  },
+  {
+    "revision": "5623404143a1b300c506",
+    "url": "/first-project/static/css/3.d0c92c80.chunk.css"
+  },
+  {
+    "revision": "238c63fcb8e93868ea6c",
+    "url": "/first-project/static/css/main.f3256555.chunk.css"
+  },
+  {
+    "revision": "970e883cbfa74d3e799c",
+    "url": "/first-project/static/js/2.f37c01b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first-project/static/js/2.f37c01b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5623404143a1b300c506",
+    "url": "/first-project/static/js/3.01bcf419.chunk.js"
+  },
+  {
+    "revision": "238c63fcb8e93868ea6c",
+    "url": "/first-project/static/js/main.9e2f2e0e.chunk.js"
+  },
+  {
+    "revision": "4177f97080b75b20d154",
+    "url": "/first-project/static/js/runtime-main.54282998.js"
+  },
+  {
+    "revision": "1a7925ff96284abba19787786c047eb5",
+    "url": "/first-project/static/media/7677edd5a44b10130b8824ca020ba60b.1a7925ff.gif"
+  },
+  {
+    "revision": "a96867e45f7ee5fd0f48b36724343771",
+    "url": "/first-project/static/media/DSC07473.a96867e4.jpg"
+  }
+]);
